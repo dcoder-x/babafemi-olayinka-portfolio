c4bc7de1077c2f51f36ed486c7d0e4c3a303cf80
dcoder-x <ayomikunfaluyi@gmail.com>
2024-03-30 18:23:02 +0100

    all done- bad commit habit

---
+++ assets/js/project-data.js
@@ -0,0 +1,11 @@
+export const projects = [
+  {
+    image: "assets/images/gamego-chart.png",
+    projectName: "GameCo",
+    Objective: `A global video game company, GameCo, is looking to use data to
+        inform development of new games. As a data analyst, I will be
+        performing descriptive analyses of video game data to better
+        understand how GameCo’s new games might fare in the market`,
+    constraint
+  },
+];
